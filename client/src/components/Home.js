6194d468288543d3ae4868ea3899f5d1f0833ec0
lyndsiWilliams <kcatgirl@gmail.com>
2020-12-10 12:26:43 -0600

    Back end message displaying on the front end!

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,20 @@
+// Package imports
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const Home = () => {
+    useEffect(() => {
+      axios.get('/api/hello')
+        .then(res => setState(res.data))
+    }, [])
+
+    const [state, setState] = useState('');
+
   return (
     <div className="home">
       <h1>Kathleen Kidder</h1>
       <h2>Writer, artist, cat and lizard fanatic</h2>
+      <p>{state}</p>
     </div>
   );
 };
